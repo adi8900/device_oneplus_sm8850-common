949fc51cc542c9f89a51fda6c491555c84eda15a
chandu078 <chandudyavanapelli03@gmail.com>
2026-03-23 15:55:26 +0000

    sm8850-common: init: Import kernel post script from vendor
    Change-Id: I0dd9f8e931dc8424e93652b380cde9e3e6c8e8d2

--- common.mk
+++ common.mk
@@ -186,7 +186,13 @@ PRODUCT_PACKAGES += \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
     init.oplus.rc \
+    init.kernel.post_boot-alor_default_6_2.sh \
+    init.kernel.post_boot-canoe_5_1.sh \
+    init.kernel.post_boot-canoe_5_2.sh \
+    init.kernel.post_boot-canoe_6_1.sh \
+    init.kernel.post_boot-canoe_default_6_2.sh \
     init.kernel.post_boot-memory.sh \
+    init.kernel.post_boot.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
