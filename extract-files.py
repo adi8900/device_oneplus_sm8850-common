2e64a5715220c4133fb4a32c7373135321c48f23
Jonas Salo <salomannen1234@gmail.com>
2026-06-28 01:00:38 +0200

    sm8850-common: add Dolby Vision media codec config
    Change-Id: Ia42c71bc8d4a6d5d46b5cf6b2e1dfb3772fae64f
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -99,11 +99,17 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'\n.*OPLUS_FEATURE_WIFI_FTM[\s\S]*?OPLUS_FEATURE_WIFI_FTM.*\n', ''),
     'vendor/etc/perf/perfboostsconfig.xml': blob_fixup()
         .regex_replace(r'Enable="false"', r'Enable="true"'),
+    # Inject Dolby Vision codec include into every non-vendor canoe variant.
     (
+        'vendor/etc/media_codecs_canoe_sku1.xml',
+        'vendor/etc/media_codecs_canoe_sku2.xml',
         'vendor/etc/media_codecs_canoe_sku3.xml',
+        'vendor/etc/media_codecs_canoe_v1.xml',
         'vendor/etc/media_codecs_canoe_v2.xml',
+        'vendor/etc/media_codecs_canoe_v3.xml',
     ): blob_fixup()
-        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', '')
+        .regex_replace(r'([ \t]*</MediaCodecs>)', r'    <Include href="media_codecs_dolby_vision.xml" />\n\1'),
     (
         'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
         'vendor/lib64/hw/libaudiocorehal.default.so',
